addeeb3b01a571ff676679276543dd5cb7b7f214
vlu00 <vlu00@stuy.edu>
2019-01-06 20:52:24 -0500

    Wrote and tested moveDown by changing moveUp

--- NumberPuzzle.java
+++ NumberPuzzle.java
@@ -142,6 +142,32 @@ public class NumberPuzzle {
 		}
 	}
 
+  public void moveDown() {
+		for (int c = 2; c < 12; c = c + 3) {
+			for (int r = 5; r > 0; r = r - 2) {
+				int orig = r;
+				if (grid[r+2][c] != ' ' && grid[r+2][c] == grid[r][c]) {
+					int newNum = (grid[r][c] - '0') * 2;
+					grid[r+2][c] = (char)(newNum + '0');
+					grid[r][c] = ' ';
+          r = r+2;
+				}
+				while (r != 7 && grid[r+2][c] == ' ') {
+					grid[r+2][c] = grid[r][c];
+					grid [r][c] = ' ';
+					r = r+2;
+					if (r != 7 && grid[r+2][c] != ' ' && grid[r+2][c] == grid[r][c]) {
+						int newNum = (grid[r][c] - '0') * 2;
+						grid[r+2][c] = (char)(newNum + '0');
+						grid[r][c] = ' ';
+						r = r+2;
+					}
+				}
+				r = orig;
+			}
+		}
+	}
+
 	public String toString() {
 		String s = "";
 		for (int r = 0; r < 9; r++) {
@@ -156,7 +182,7 @@ public class NumberPuzzle {
 	public static void main(String[] args) {
 		NumberPuzzle A = new NumberPuzzle();
 		System.out.println(A.toString());
-		A.moveUp();
+		A.moveDown();
 		System.out.println(A.toString());
 	}
 
