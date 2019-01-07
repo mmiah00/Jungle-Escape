1b2472fb6e72a432a7b612de0066c9ab0888c434
mmiah <mmiah00@stuy.edu>
2019-01-07 15:29:41 -0500

    made public fields private

--- Tile.java
+++ Tile.java
@@ -1,12 +1,28 @@
 public class Tile {
-  char tens, ones; //num 1 is 0 or 1 for the tens digit and num 2 is the ones digit
-  int x,y;
+  private char tens, ones; //num 1 is 0 or 1 for the tens digit and num 2 is the ones digit
+  private int x,y;
 
   public Tile (char num1, char num2, int xcor, int ycor) {
   	tens = num1;
     ones = num2;
   	x = xcor;
   	y = ycor;
+  }
+
+  public int xcor () {
+    return x;
+  }
+
+  public int ycor () {
+    return y;
+  }
+
+  public char tens () {
+    return tens;
+  }
+
+  public char ones () {
+    return ones;
   }
 
   public String toString () {
