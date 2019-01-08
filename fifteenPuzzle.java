8f312c08bc91e0439fd0a25dc1f7987ac3eccfbc
mmiah <mmiah00@stuy.edu>
2019-01-07 22:59:20 -0500

    need to write key controls

--- fifteenPuzzle.java
+++ fifteenPuzzle.java
@@ -1,117 +1,86 @@
-//API : http://mabe02.github.io/lanterna/apidocs/2.1/
-import com.googlecode.lanterna.terminal.Terminal.SGR;
-import com.googlecode.lanterna.TerminalFacade;
-import com.googlecode.lanterna.input.Key;
-import com.googlecode.lanterna.input.Key.Kind;
-import com.googlecode.lanterna.terminal.Terminal;
-import com.googlecode.lanterna.terminal.Terminal.Color;
-import com.googlecode.lanterna.terminal.TerminalSize;
-import com.googlecode.lanterna.LanternaException;
-import com.googlecode.lanterna.input.CharacterPattern;
-import com.googlecode.lanterna.input.InputDecoder;
-import com.googlecode.lanterna.input.InputProvider;
-import com.googlecode.lanterna.input.Key;
-import com.googlecode.lanterna.input.KeyMappingProfile;
+import com.googlecode.lanterna.*;
+import com.googlecode.lanterna.input.*;
+import com.googlecode.lanterna.terminal.*;
+import com.googlecode.lanterna.screen.*;
+import java.io.IOException;
+import java.awt.Color;
 
 
 public class FifteenPuzzle {
-  Tile[] nums;
+  private static Tile[] nums;
+  private static boolean done;
 
   public FifteenPuzzle () {
+    done = false;
     nums = new Tile [16];
-    int xcor = 10;
-    int ycor = 10;
-    nums[8] = new Tile ('0', '1', xcor, ycor);
-    xcor += 5;
-    nums[1] = new Tile ('0', '2', xcor, ycor);
-    xcor += 5;
-    nums[12] = new Tile ('0', '3', xcor, ycor);
-    xcor += 5;
-    nums[3] = new Tile ('0', '4', xcor, ycor);
-    xcor += 5;
-    nums[13] = new Tile ('0', '5', xcor, ycor);
-    xcor = 10;
-    ycor += 5;
-    nums[11] = new Tile ('0', '6', xcor, ycor);
-    xcor += 5;
-    nums[6] = new Tile ('0', '7', xcor, ycor);
-    xcor += 5;
-    nums[14] = new Tile ('0', '8', xcor, ycor);
-    xcor += 5;
-    nums[5] = new Tile ('0', '9', xcor, ycor);
-    xcor = 10;
-    ycor += 5;
-    nums[0] = new Tile ('1', '0', xcor, ycor);
-    xcor += 5;
-    nums[9] = new Tile ('1', '1', xcor, ycor);
-    xcor += 5;
-    nums[4] = new Tile ('1', '2', xcor, ycor);
-    xcor += 5;
-    nums[2] = new Tile ('1', '3', xcor, ycor);
-    xcor = 10;
-    ycor += 5;
-    nums[10] = new Tile ('1', '4', xcor, ycor);
-    xcor += 5;
-    nums[7] = new Tile ('1', '5', xcor, ycor);
-    xcor += 5;
-    nums[15] = new Tile (' ', xcor, ycor);
 
+    nums[0] = new Tile ('1', '0', 10, 10);
+    nums[1] = new Tile ('0', '2', 21, 10);
+    nums[2] = new Tile ('1', '3', 32, 10);
+    nums[3] = new Tile ('0', '4', 43, 10);
+
+    nums[4] = new Tile ('1', '2', 10, 15);
+    nums[5] = new Tile ('0', '9', 21, 15);
+    nums[6] = new Tile ('0', '7', 32, 15);
+    nums[7] = new Tile ('1', '5', 43, 15);
+
+    nums[8] = new Tile ('0', '1', 10, 20);
+    nums[9] = new Tile ('1', '1', 21, 20);
+    nums[10] = new Tile ('1', '4', 32, 20);
+    nums[11] = new Tile ('0', '6', 43, 20);
+
+    nums[12] = new Tile ('0', '3', 10, 25);
+    nums[13] = new Tile ('0', '5', 21, 25);
+    nums[14] = new Tile ('0', '8', 32, 25);
+    nums[15] = new Tile (' ', ' ', 43, 25);
   }
 
-  public static void putString (Tile aTile) {
+  /*
+  public static void putString (Tile aTile, Terminal t) {
     t.moveCursor (aTile.xcor(), aTile.ycor());
     String s = aTile.toString ();
     for (int i = 0; i < s.length (); i ++) {
       t.putCharacter(s.charAt (i));
     }
   }
+  */
 
-  public boolean complete () {
+  public static void putString(Tile aTile, Screen screen) {
+    String str = aTile.toString ();
+    int x = aTile.xcor ();
+    int y = aTile.ycor ();
+		for (int i = 0; i < str.length(); ++i) {
+			screen.setCharacter(x+i, y, new TextCharacter(str.charAt(i)));
+		}
+	}
+
+  public void complete () {
+    String actual = "0102030405060708091011131415  ";
+    String ans = "";
+    for (int i = 0; i < nums.length; i ++) {
+      ans += nums[i].tens () + nums[i].ones ();
+    }
+    done = (actual == ans);
   }
 
-  public static void main(String[] args) {
-   	 Terminal terminal = TerminalFacade.createTextTerminal();
-   	 terminal.enterPrivateMode();
+  public static void main(String[] args) throws IOException {
+    FifteenPuzzle board = new FifteenPuzzle ();
 
-   	 TerminalSize size = terminal.getTerminalSize();
-   	 terminal.setCursorVisible(false);
+    int x = board.nums[15].xcor();
+    int y = board.nums[15].ycor();
 
-   	 boolean running = true;
+     Screen screen = new DefaultTerminalFactory().createScreen();
+     screen.startScreen();
 
-   	 long tStart = System.currentTimeMillis();
-   	 long lastSecond = 0;
+     long tStart = System.currentTimeMillis();
+     long lastSecond = 0;
 
-   	 while(running){
+   	 while(!done){
+       for (int i = 0; i < board.nums.length; i ++) {
+         putString (board.nums[i], screen);
+       }
 
-   		 terminal.moveCursor(x,y);
-   		 terminal.applyBackgroundColor(Terminal.Color.WHITE);
-   		 terminal.applyForegroundColor(Terminal.Color.BLACK);
-   		 //applySGR(a,b) for multiple modifiers (bold,blink) etc.
-   		 terminal.applySGR(Terminal.SGR.ENTER_UNDERLINE);
-   		 terminal.putCharacter('\u00a4');
-   		 //terminal.putCharacter(' ');
-   		 terminal.applyBackgroundColor(Terminal.Color.DEFAULT);
-   		 terminal.applyForegroundColor(Terminal.Color.DEFAULT);
-   		 terminal.applySGR(Terminal.SGR.RESET_ALL);
-
-
-   		 terminal.moveCursor(size.getColumns()-5,5);
-   		 terminal.applyBackgroundColor(Terminal.Color.RED);
-   		 terminal.applyForegroundColor(Terminal.Color.YELLOW);
-   		 terminal.applySGR(Terminal.SGR.ENTER_BOLD);
-   		 terminal.putCharacter(' ');
-   		 terminal.putCharacter(' ');
-   		 terminal.putCharacter('\u262d');
-   		 terminal.putCharacter(' ');
-   		 terminal.moveCursor(size.getColumns()-5,6);
-   		 terminal.putCharacter(' ');
-   		 terminal.putCharacter(' ');
-   		 terminal.putCharacter(' ');
-   		 terminal.putCharacter(' ');
-   		 terminal.applyBackgroundColor(Terminal.Color.DEFAULT);
-   		 terminal.applyForegroundColor(Terminal.Color.DEFAULT);
-
-   		 Key key = terminal.readInput();
+       /*KeyStroke key = screen.pollInput();
 
    		 if (key != null)
    		 {
@@ -119,7 +88,6 @@ public class FifteenPuzzle {
    			 if (key.getKind() == Key.Kind.Escape) {
 
    				 terminal.exitPrivateMode();
-   				 running = false;
    			 }
 
    			 if (key.getKind() == Key.Kind.ArrowLeft) {
@@ -163,39 +131,8 @@ public class FifteenPuzzle {
    			 lastSecond = millis / 1000;
    			 //one second has passed.
    			 putString(1,3,terminal,"Seconds since start of program: "+lastSecond);
-   		 }
+   		 }*/
    	 }
-    }
+
+   }
 }
-
-/*
-for (int x = 1; x - 1 < 16; x ++) {
-  if (x == 5 || x == 9 || x == 13) {
-    xcor = 10;
-    ycor += 5;
-  }
-
-  if (x <= 4 && x >= 1) {
-    nums [x] = new Tile ((char) x, xcor, ycor);
-    xcor += 5;
-  }
-
-  if (x <= 5 && x >= 8) {
-    nums [x] = new Tile ((char) x, xcor, ycor);
-    xcor += 5;
-  }
-
-  if (x <= 9 && x >= 12) {
-    nums [x] = new Tile ((char) x, xcor, ycor);
-    xcor += 5;
-  }
-
-  if (x <= 13 && x >= 15) {
-    nums [x] = new Tile ((char) x, xcor, ycor);
-    xcor += 5;
-  }
-
-}
-nums [15] = new Tile (' ', xcor, ycor);
-}
-*/
