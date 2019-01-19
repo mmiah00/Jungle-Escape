f712d1d79300c3810e60c1dbb65a9f50a4928cf6
mmiah <mmiah00@stuy.edu>
2019-01-18 20:22:23 -0500

    added new files for it to commit

--- compile.sh
+++ compile.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+javac -cp lanterna.jar:. Scene1.java
+javac -cp lanterna.jar:. Scene2.java
+javac -cp lanterna.jar:. Scene3.java
+javac -cp lanterna.jar:. NumberPuzzle.java
+javac -cp lanterna.jar:. SecondPuzzle.java
+javac -cp lanterna.jar:. Frogger.java
 javac -cp lanterna.jar:. JungleEscape.java
-
