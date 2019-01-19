6147abe1509ebbde482555eba3434481118d8c23
vlu00 <vlu00@stuy.edu>
2019-01-18 22:41:13 -0500

    Cars properly printed when constructor is called. Need to make cars move

--- Frogger.java
+++ Frogger.java
@@ -1,15 +1,20 @@
 import java.util.*;
 
 public class Frogger {
-  private String[10][8] world;
+  private String[][] world;
   private int lives;
   private int currentRow;
   private int currentCol;
 
   public Frogger() {
+    world = new String[10][8];
+    for (int c = 0; c < 8; c++) {
+      world[0][c] = "***";
+      world[9][c] = "***";
+    }
     for (int r = 1; r < 9; r++) {
       for (int c = 0; c < 8; c++) {
-        world[r][c] = "     \n     ";
+        world[r][c] = "   ";
       }
       addCars(r);
     }
@@ -19,36 +24,33 @@ public class Frogger {
 
   public void addCars(int r) {
     Random randgen = new Random();
-    for (int i = 0; i < 4; i++) {
-      int randCol;
-      while (!(world[r][randCol].equals("     \n     "))) {
-        randCol = randgen.nextInt(8);
-      }
-      world[r][randCol] = "[ = ]\n o o ";
+    for (int i = 0; i < 3; i++) {
+      int randCol = randgen.nextInt(8);
+      world[r][randCol] = "o-o";
     }
   }
 
   public void moveCarsLeft(int r) {
     for (int c = 0; c < 8; c++) {
-      if (world[r][c].equals("[ = ]\n o o ")) {
-        world[r][c-1] = "[ = ]\n o o ";
-        world[r][c] = "     \n     ";
+      if (world[r][c].equals("o-o")) {
+        world[r][c-1] = "o-o";
+        world[r][c] = "   ";
       }
     }
   }
 
   public void moveCarsRight(int r) {
     for (int c = 0; c < 8; c++) {
-      if (world[r][c].equals("[ = ]\n o o ")) {
-        world[r][c+1] = "[ = ]\n o o ";
-        world[r][c] = "     \n     ";
+      if (world[r][c].equals("o-o")) {
+        world[r][c+1] = "o-o";
+        world[r][c] = "   ";
       }
     }
   }
 
-  public void movePlayer(int horizontal, int vertical) {
-    
-  }
+  //public void movePlayer(int horizontal, int vertical) {
+
+  //}
 
   public String toString() {
     String s = "";
@@ -56,11 +58,13 @@ public class Frogger {
       for (int c = 0; c < 8; c++) {
         s = s + world[r][c];
       }
+      s = s + "\n";
     }
     return s;
   }
 
   public static void main (String args[]) {
-
+    Frogger A = new Frogger();
+    System.out.println(A.toString());
   }
 }
