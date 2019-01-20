7f3f7d93ad3ed2ea8d8b9e9e88e9a28f85a80e2f
mmiah <mmiah00@stuy.edu>
2019-01-20 15:54:12 -0500

    adding color

--- Scene2.java
+++ Scene2.java
@@ -18,7 +18,7 @@ public class Scene2 extends Scene{
   public Scene2 (Terminal t) {
     path = new String[6];
     putString(0, 0, t, "------------------------");
-    putString(0, 1, t, "            /   .\\      ");
+    putString(12, 1, t, "/   .\\", Terminal.Color.DEFAULT, Terminal.Color.BLUE);
     putString(0, 2, t, "           / ` .  \\     ");
     putString(0, 3, t, "          / .   `  \\    ");
     putString(0, 4, t, "         /.   `  `  \\   ");
@@ -40,6 +40,18 @@ public class Scene2 extends Scene{
     putString(0, 12, t, "| Solve the puzzle to open |");
     putString(0, 13, t, "| the box and get materials|");
     putString(0, 14, t, "|    to build a bridge.    |");
+  }
+
+  public static void putString(int r, int c,Terminal t, String s, Terminal.Color forg, Terminal.Color back ){
+    t.moveCursor(r,c);
+    t.applyBackgroundColor(forg);
+    t.applyForegroundColor(Terminal.Color.BLACK);
+
+    for(int i = 0; i < s.length();i++){
+      t.putCharacter(s.charAt(i));
+    }
+    t.applyBackgroundColor(Terminal.Color.DEFAULT);
+    t.applyForegroundColor(Terminal.Color.DEFAULT);
   }
 
   public static int playScene2(Terminal terminal) {
