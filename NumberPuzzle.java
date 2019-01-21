5551cf9fbac6da001b29660db361fe3331260255
mmiah <mmiah00@stuy.edu>
2019-01-20 21:38:23 -0500

    fixed so the game doesn't stop after 16

--- NumberPuzzle.java
+++ NumberPuzzle.java
@@ -193,7 +193,7 @@ public class NumberPuzzle {
 	public boolean beatGame() {
     for (int r = 0; r < 4; r++) {
       for (int c = 0; c < 4; c++) {
-				if (grid[r][c].equals(" 16")) {
+				if (grid[r][c].equals(" 256")) {
 					return true;
 				}
 			}
