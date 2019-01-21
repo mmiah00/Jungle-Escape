9217b9a6f5bd7865c405399ad148ea0dc6eb8cdb
vlu00 <vlu00@stuy.edu>
2019-01-21 12:06:53 -0500

    testing Scene1 timer

--- Scene1.java
+++ Scene1.java
@@ -71,7 +71,13 @@ public class Scene1 extends Scene {
       timer += (currentTime -lastTime);
       putString(0,1,terminal, "Time: " + timer);
       long minPassed = 14 - timer/60000;
-      long secPassed= 60 - ((timer%60000)/1000);
+      long secPassed;
+      if (timer/60000 == 0) {
+        secPassed = 60 - timer/1000;
+      }
+      else {
+        secPassed = 60 - timer%60000/1000;
+      }
       putString(0,0,terminal, "Time: "+ minPassed + ":" + secPassed);
     }
     terminal.clearScreen();
